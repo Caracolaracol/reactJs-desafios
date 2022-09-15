3d50147f08ab81eb181d758f89e90ae862313aa4
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-09-14 23:44:42 -0300

    itemlistcontainer

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <ItemListContainer />
+    <ItemListContainer greeting="Productos en venta" />
   </React.StrictMode>
 );
 
